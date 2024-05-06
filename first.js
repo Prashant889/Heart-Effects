930c1da10ba88f08790e3f15a9010a6807360b6f
Prashant Kumar Gautam <163720886+Prashant889@users.noreply.github.com>
2024-05-06 18:27:02 +0530

    Update first.js

--- first.js
+++ first.js
@@ -7,7 +7,7 @@ body.addEventListener("mousemove", (e)=> {
     let spanEl = document.createElement("span");
     spanEl.style.left = xPos + "px";
     spanEl.style.top = yPos + "px";
-    let size = Math.random() * 100;
+    let size = Math.random() * 100; //point ke pahle 2 digit tak lane ke liye
     spanEl.style.width = size + "px";
     spanEl.style.height = size + "px"
     body.appendChild(spanEl);
